08857b113af7296f998d93922dd9a5e812be5708
Arpad Kucserak <kucserak@gmail.com>
2020-01-30 19:49:29 +0100

    Add project files.

---
+++ FileConverterService/ConverterService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Topshelf.Logging;
+
+namespace FileConverterService
+{
+    class ConverterService
+    {
+        private FileSystemWatcher _watcher;
+        private static readonly LogWriter _log = HostLogger.Get<ConverterService>();
+
+        public bool Start()
+        {
+            _watcher = new FileSystemWatcher(@"c:\temp\a", "*_in.txt");
+
+            _watcher.Created += FileCreated;
+
+            _watcher.IncludeSubdirectories = false;
+
+            _watcher.EnableRaisingEvents = true;
+
+            return true;
+        }
+
+        public bool Pause()
+        {
+            _watcher.EnableRaisingEvents = false;
+            return true;
+        }
+
+        public bool Continue()
+        {
+            _watcher.EnableRaisingEvents = true;
+            return true;
+        }
+
+
+        public void CustomCommand(int commandNumber)
+        {
+            _log.InfoFormat("Hey, I got the command number '{0}'", commandNumber);
+        }
+
+        private void FileCreated(object sender, FileSystemEventArgs e)
+        {
+            _log.InfoFormat("Starting conversion of '{0}'", e.FullPath);
+
+            if (e.FullPath.Contains("bad_in"))
+            {
+                throw new NotSupportedException("Cannot convert");
+            }
+
+            string content = File.ReadAllText(e.FullPath);
+
+            string upperContent = content.ToUpperInvariant();
+
+            var dir = Path.GetDirectoryName(e.FullPath);
+
+            var convertedFileName = Path.GetFileName(e.FullPath) + ".converted";
+
+            var convertedPath = Path.Combine(dir, convertedFileName);
+
+            File.WriteAllText(convertedPath, upperContent);
+        }
+
+        public bool Stop()
+        {
+            _watcher.Dispose();
+
+            return true;
+        }
+    }
+}
---
+++ FileConverterService/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace FileConverterService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(serviceConfig =>
+        {
+            serviceConfig.UseNLog();
+
+            serviceConfig.Service<ConverterService>(serviceInstance =>
+            {
+                serviceInstance.ConstructUsing(
+                    () => new ConverterService());
+
+                serviceInstance.WhenStarted(
+                    execute => execute.Start());
+
+                serviceInstance.WhenStopped(
+                    execute => execute.Stop());
+
+                serviceInstance.WhenPaused(execute => execute.Pause());
+
+                serviceInstance.WhenContinued(execute => execute.Continue());
+
+                serviceInstance.WhenCustomCommandReceived(
+                    (execute, hostControl, commandNumber) =>
+                        execute.CustomCommand(commandNumber));
+            });
+
+            serviceConfig.EnableServiceRecovery(recoveryOption =>
+            {
+                recoveryOption.RestartService(1);
+                recoveryOption.RestartComputer(60, "PS Demo");
+                recoveryOption.RunProgram(5,
+                    @"c:\someprogram.exe");
+            });
+                                
+                                
+            serviceConfig.EnablePauseAndContinue();
+
+            serviceConfig.SetServiceName("AwesomeFileConverter");
+            serviceConfig.SetDisplayName("Awesome File Converter");
+            serviceConfig.SetDescription("A Pluralsight demo service");
+
+            serviceConfig.StartAutomatically();
+        });
+        }
+    }
+}
